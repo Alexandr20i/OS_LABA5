4f7db07e0106509bfdff11f87a8b6aa5b5135304
Alexandr20i <Alexandr20i@yandex.ru>
2023-04-30 15:38:49 +0400

    Добавьте файлы проекта.

---
+++ client/client.cpp
@@ -0,0 +1,59 @@
+﻿#include <iostream>
+#include <winsock2.h>
+
+using namespace std;
+
+#pragma warning(disable: 4996)
+#pragma comment(lib, "ws2_32.lib") // Для работы с сокетами
+
+SOCKET Connection;
+
+// принятие сообщения от сервера
+void ClientHandler() {
+	char msg[256];
+	while (true) {
+		recv(Connection, msg, sizeof(msg), NULL); // получение информации 
+		cout << msg << endl;
+	}
+}
+
+int main() {
+
+	WSADATA wsaData; // создаём структуру wsaData
+	WORD DLLVersion = MAKEWORD(2, 1); // используется для указания версии Winsock, которая будет запрошена при инициализации библиотеки с помощью функции WSAStartup
+	if (WSAStartup(DLLVersion, &wsaData) != 0) { // проверка на инициализацию библиотеки Winsock
+		cout << "Error for inicialization Winsock! " << endl;
+		exit(1);
+	}
+
+	SOCKADDR_IN addr; // структура для хранение адреса
+	int sizeofaddr = sizeof(addr); //размер
+	addr.sin_addr.s_addr = inet_addr("127.0.0.1"); // IP-адрес сервера (локал хост)
+	addr.sin_port = htons(1111); // Порт, на котором будет слушать сервер
+	addr.sin_family = AF_INET;   // семейство протоколов, для интерент протоколов: AF_INET
+
+	Connection = socket(AF_INET, SOCK_STREAM, NULL);
+	if (connect(Connection, (SOCKADDR*)&addr, sizeof(addr)) != 0){ //проверка на подключение к серверу 
+		cout << "Error: failed connect to server.\n";
+		return 1;
+	}
+	
+	cout << "Connected!\n"; //подключился
+	/* // уже не надо, так как это делает void ClientHandler, запущенная в новом потоке 
+	char msg[256];
+	recv(Connection, msg, sizeof(msg), NULL);
+	cout << msg << endl;
+	*/
+
+	CreateThread(NULL, NULL, (LPTHREAD_START_ROUTINE)ClientHandler, NULL, NULL, NULL);
+
+	char msgl[256];
+	while (true) {
+		cin.getline(msgl, sizeof(msgl));			
+		send(Connection, msgl, sizeof(msgl), NULL);
+		Sleep(3);
+	}
+	
+	system("pause");
+	return 0;
+}
---
+++ server/server.cpp
@@ -0,0 +1,70 @@
+﻿#include <iostream>
+#include <winsock2.h>
+
+#pragma warning(disable: 4996)
+#pragma comment(lib, "ws2_32.lib") // Для работы с сокетами
+
+using namespace std;
+
+SOCKET Connections[100]; // массив сокетов 
+int Counter = 0; // иднекс массива(сокета/клиента)
+
+void ClientHandler(int index) {
+	char msg[256];
+	while (true) {
+		recv(Connections[index], msg, sizeof(msg), NULL); // принимает сообщение клиента
+		for (int i = 0; i < Counter; ++i) {
+			if (i == index)
+				continue;
+			send(Connections[i], msg, sizeof(msg), NULL); // отправляет клиенту 
+		}
+	}
+}
+
+int main() {
+	WSADATA wsaData; // создаём структуру wsaData
+	WORD DLLVersion = MAKEWORD(2, 1); // используется для указания версии Winsock, которая будет запрошена при инициализации библиотеки с помощью функции WSAStartup
+	if (WSAStartup(DLLVersion, &wsaData) != 0) { // проверка на инициализацию библиотеки Winsock
+		cout << "Error for inicialization Winsock! " << endl;
+		exit(1);
+	}
+
+	SOCKADDR_IN addr; // структура для хранение адреса
+	int sizeofaddr = sizeof(addr); //размер
+	addr.sin_addr.s_addr = inet_addr("127.0.0.1"); // IP-адрес сервера (локал хост)
+	//addr.sin_addr.s_addr = INADDR_ANY;
+	addr.sin_port = htons(1111); // Порт, на котором будет слушать сервер
+	addr.sin_family = AF_INET;   // семейство протоколов, для интерент протоколов: AF_INET
+
+	SOCKET slisten = socket(AF_INET, SOCK_STREAM, NULL); //сокет для прослушивания порта
+	if (bind(slisten, (SOCKADDR*)&addr, sizeof(addr)) != 0) {		 //проверка на привязку адреса сокету
+		closesocket(slisten);
+		WSACleanup();
+	}
+	int c = 3;
+	listen(slisten, c); // прослушивание, сколько запросов ожидается, (остальные получат ошибку)
+
+	cout << "Waiting for client connection..." << endl;
+
+	SOCKET newConnection;
+	//newConnection = accept(slisten, (SOCKADDR*)&addr, &sizeofaddr); // 
+	for (int i = 1; i <= c; ++i) {
+		newConnection = accept(slisten, (SOCKADDR*)&addr, &sizeofaddr);
+		if (newConnection == 0)
+			cout << "Error conect with client" << endl;
+		else {
+			cout << "Client " << i << " connected!" << endl;
+			// дальше идёт межсетевое взаимодействие
+
+
+			char msg[256] = "From server! \n";
+			send(newConnection, msg, sizeof(msg), NULL); // отправка клиенту
+
+			Connections[i] = newConnection;
+			++Counter;
+			CreateThread(NULL, NULL, (LPTHREAD_START_ROUTINE) ClientHandler, (LPVOID)(i), NULL, NULL); // создаём новый поток, в котором будет выполняться функция ClientHandler
+		}
+	}
+	system("pause");
+	return 0;
+}
